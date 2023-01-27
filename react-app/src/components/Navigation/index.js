6162b7b1145fe88d0ed27f9bba7d2aa3df55260f
eric chai <no8cai@gmail.com>
2023-01-27 03:13:29 -0600

    topbar css

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -6,6 +6,7 @@ import ProfileButton from './ProfileButton';
 import OpenModalButton from '../OpenModalButton';
 import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
+import logo from './atlogo.png'
 
 function Navigation(){
   
@@ -34,8 +35,8 @@ function Navigation(){
   let addressbar;
   if (sessionUser) {
     addressbar = (
-      <div>
-        <i className="fa-solid fa-location-dot"></i><div>deliver to {`${sessionUser.city} ${sessionUser.zipcode}`}</div>
+      <div className='navigation-leftright'>
+        <i className="fa-solid fa-location-dot"></i><div>Deliver to {`${sessionUser.username}, ${sessionUser.city} ${sessionUser.zipcode}`}</div>
       </div>
     );
   } else {
@@ -56,8 +57,8 @@ function Navigation(){
   return (
     <div className='navigation-section'>
       <div className='navigation-leftsec'>
-        <div>
-         <NavLink exact to="/" className={'navigation-logo'}><img src="atlogo.png"/></NavLink>
+        <div className='topbutton'>
+         <NavLink exact to="/" className={'navigation-logo'}><img className='navigation-sublogo' src={logo}/></NavLink>
         </div>
         <div className='navigation-address'>
           {addressbar}
@@ -65,15 +66,15 @@ function Navigation(){
       </div>
       <div className="search-container">
       <form action="/action_page.php" className='form-input'>
-      <input type="text" placeholder="Search.." name="search" disabled="True" title="Feature coming soon!"/>
-      <button type="submit" disabled="True" title="Feature coming soon!"><i className="fa fa-search" ></i></button>
+      <input type="text" placeholder="Search Atlentis" name="search" disabled="True" title="Feature coming soon!" id='do-not-interact'/>
+      <button type="submit" disabled="True" title="Feature coming soon!"><i className="fa fa-search" id='do-not-interact'></i></button>
       </form>
      </div>
       <div className='navigation-rightsec'>
            <div><ProfileButton user={sessionUser} /></div>
-           <div>Returns&Orders</div>
+           <div className='ng-secondsec' id='do-not-interact'><div className='profile-context'>Returns</div> <div className='np-seconddown'>& Orders</div></div>
            <div>
-           <NavLink exact to="/cartitems"><i className="fa-solid fa-cart-arrow-down"></i></NavLink>
+           <NavLink exact to="/cartitems" className={'ng-thirdsec'}><i className="fa-solid fa-cart-arrow-down"></i></NavLink>
             </div>
       </div>
       {/* <div>{isLoaded && sessionLinks}</div> */}
